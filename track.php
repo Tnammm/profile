36d85df1102e3d4183daca8625ac85c33a1522f1
Tnammm <130284813+Tnammm@users.noreply.github.com>
2024-09-14 19:57:55 +0700

    Update track.php

--- track.php
+++ track.php
@@ -1,13 +1,6 @@
 <?php
-// File to store the view count
 $file = 'views.txt';
-
-// Read the current count
 $current = file_exists($file) ? (int)file_get_contents($file) : 0;
-
-// Increment the count
 $current++;
-
-// Save the new count
 file_put_contents($file, $current);
 ?>
